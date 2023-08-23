79faad6f73bf01cf7e585b33d1bc1c2d78139458
Christos Chousiadas <chousiadas6395@gmail.com>
2023-08-23 18:56:09 +0300

    Add custom error responses

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,6 +9,12 @@ func (app *application) routes() *httprouter.Router {
 	// Initialize a new http-router router instance.
 	router := httprouter.New()
 
+	// Customize http-router error responses
+	// Convert the notFoundResponse() and methodNotAllowedResponse helper to a http.Handler
+	// using the http.HandlerFunc() adapter
+	router.NotFound = http.HandlerFunc(app.notFoundResponse)
+	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
+
 	// Register the relevant methods, URL patterns and handler functions for our endpoints.
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
