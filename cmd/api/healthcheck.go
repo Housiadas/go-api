d624b3752d23d33d9fffe11ee86122d797feb3c9
Christos Chousiadas <chousiadas6395@gmail.com>
2023-08-23 17:10:31 +0300

    Fix sync error for auto build

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,16 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 // Declare a handler which writes a plain-text response
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	js := `{"status": "available", "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
-	// Set the "Content-Type: application/json" header on the response.
+	// Create a map which holds the information that we want to send in the response.
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
+
+	// Append a newline to the JSON. This is just a small nicety to make it easier to view in terminal applications.
+	js = append(js, '\n')
 	w.Header().Set("Content-Type", "application/json")
-	// Write the JSON as the HTTP response body.
-	w.Write([]byte(js))
+	_, err = w.Write(js)
+	if err != nil {
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
 }
